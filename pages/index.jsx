2e8a9c7228436ea74d55fd439914faed5c827e01
simret2 <5@afrika.dev>
2022-09-09 22:10:54 +0300

    first commit

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import Gallery from "../components/gallery";
+import Gallery from "../components/Gallery";
 
 const Home = () => {
   const [wallet, setWalletAddress] = useState("");
